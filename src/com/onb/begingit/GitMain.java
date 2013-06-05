bae7968722a67b57562f91e15d21d1be4030741f
Training <training@bootcamp-wheezy>
2013-06-05 19:44:11 +0800

    Initial Commit

---
+++ src/com/onb/begingit/GitMain.java
@@ -0,0 +1,15 @@
+package com.onb.begingit;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class GitMain {
+
+	@Test
+	public void helloWorld(){
+		Assert.assertEquals(false, false);
+	}
+	
+	
+}
